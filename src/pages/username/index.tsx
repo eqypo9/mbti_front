aa91748e828b6e1d5752f492d3077057f772e197
eqypo9 <eqypo@naver.com>
2025-02-28 14:39:03 +0900

    feat: 프로필 이미지 안내 텍스트 추가

--- src/pages/username/index.tsx
+++ src/pages/username/index.tsx
@@ -31,19 +31,28 @@ export default function UsernamePage() {
   };
 
   return (
-    <div className='w-full p-6 flex flex-col justify-center items-center space-y-6'>
-      <h1 className='text-2xl font-bold text-white'>
+    <div className='w-full p-4 sm:p-6 flex flex-col justify-center items-center space-y-6'>
+      <h1 className='sm:text-2xl text-xl font-bold text-white'>
         소녀의 이름은 무엇인가요?
       </h1>
 
-      {/* 프로필 이미지 미리보기 */}
-      <label htmlFor='profile-upload' className='cursor-pointer'>
-        <div className='w-36 h-36 rounded-full border-4 border-gray-400 overflow-hidden'>
+      {/* 프로필 이미지 업로드 안내 텍스트 */}
+      <p className='text-sm text-gray-300'>
+        클릭하여 프로필 사진을 업로드하세요!
+      </p>
+
+      <label htmlFor='profile-upload' className='relative cursor-pointer'>
+        {/* 프로필 이미지 박스 */}
+        <div className='sm:w-36 w-28 sm:h-36 h-28 rounded-full border-4 border-gray-400 overflow-hidden relative'>
           <img
             src={profileImage}
             alt='Profile Preview'
             className='w-full h-full object-cover scale-110 block'
           />
+
+          <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity'>
+            <span className='text-white text-sm font-semibold'>변경하기</span>
+          </div>
         </div>
       </label>
 
@@ -58,7 +67,7 @@ export default function UsernamePage() {
 
       <input
         type='text'
-        className='w-full px-4 py-2 border border-gray-300 rounded-md text-lg text-black'
+        className='w-full px-4 py-2 sm:text-lg text-base border border-gray-300 rounded-md text-black'
         placeholder='이름을 입력하세요'
         value={name}
         onChange={(e) => setName(e.target.value)}
@@ -68,7 +77,7 @@ export default function UsernamePage() {
       />
 
       <button
-        className='px-6 py-3 bg-white text-black font-medium text-lg rounded-md hover:bg-gray-300'
+        className='sm:px-6 px-4 sm:py-3 py-2 bg-white text-black font-medium sm:text-lg text-base rounded-md hover:bg-gray-300'
         onClick={handleSubmit}
       >
         제출하기
