6cfd291bfb7ed078c61c61d9a4c2e962456ff785
eqypo9 <eqypo@naver.com>
2025-02-28 14:33:24 +0900

    feat: 랜딩 페이지 반응형 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,19 @@ import Image from 'next/image';
 
 export default function LandingPage() {
   return (
-    <div className='flex flex-col justify-center items-center space-y-6'>
-      <h1 className='text-4xl font-bold text-white'>GANEUNGHAN</h1>
+    <div className='flex flex-col justify-center items-center space-y-6 p-4'>
+      <h1 className='sm:text-4xl text-2xl font-bold text-white'>GANEUNGHAN</h1>
 
       <Image
         src='/images/test-wolf.png'
         alt='Main'
         width={240}
         height={240}
-        className='object-contain'
+        className='object-contain sm:w-[240px] w-[180px]'
       />
 
       <Link href='/username'>
-        <button className='px-6 py-3 bg-white text-black font-medium text-lg rounded-md hover:bg-gray-300'>
+        <button className='sm:px-6 px-4 sm:py-3 py-2 bg-white text-black font-medium sm:text-lg text-base rounded-md hover:bg-gray-300'>
           Press Start
         </button>
       </Link>
