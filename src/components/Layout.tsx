f3885cbb5226b257a085b8ddc847667d165d1198
eqypo9 <eqypo@naver.com>
2025-02-28 16:55:08 +0900

    fix: 모바일 일때 사진 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className='min-h-screen flex items-center justify-center bg-white'>
-      <div className='w-full max-w-[600px] h-screen bg-black text-white flex flex-col justify-center items-center overflow-auto sm:p-6 p-4'>
+      <div className='w-full max-w-[600px] h-screen bg-black text-white flex flex-col justify-center items-center overflow-auto'>
         {children}
       </div>
     </div>
--- src/components/SlideContent.tsx
+++ src/components/SlideContent.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 interface SlideContentProps {
   currentSlide: {
@@ -8,12 +9,29 @@ interface SlideContentProps {
     description?: string;
   };
 }
+
 export function SlideContent({ currentSlide }: SlideContentProps) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='relative w-full h-full'>
       {currentSlide.type === 'image' ? (
         <Image
-          src={currentSlide.src ?? '/images/test-cake-jo.png'}
+          src={
+            isMobile
+              ? '/images/test-cake-jo-mobile.png'
+              : currentSlide.src ?? '/images/test-cake-jo.png'
+          }
           alt='결과 이미지'
           layout='fill'
           objectFit='cover'
