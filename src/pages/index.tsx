a992edd3b2e62097533d914a220cdf927fd7695e
eqypo9 <eqypo@naver.com>
2025-02-28 13:45:12 +0900

    style: 폰트 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function LandingPage() {
   return (
     <div className='flex flex-col justify-center items-center space-y-6'>
-      <h1 className='text-4xl font-bold text-white'>테스트 페이지</h1>
+      <h1 className='text-4xl font-bold text-white'>GANEUNGHAN</h1>
 
       <Image
         src='/images/test-main.png'
@@ -16,7 +16,7 @@ export default function LandingPage() {
 
       <Link href='/username'>
         <button className='px-6 py-3 bg-white text-black font-medium text-lg rounded-md hover:bg-gray-300'>
-          테스트 시작하기
+          Press Start
         </button>
       </Link>
     </div>
